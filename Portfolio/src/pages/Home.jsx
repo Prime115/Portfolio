e12982ab5479ec0b968d30d611fcc780812c7661
lundg <id23oln@cs.umu.se>
2025-12-04 20:56:03 +0100

    Added a typing text animation for the home page

--- Portfolio/src/pages/Home.jsx
+++ Portfolio/src/pages/Home.jsx
@@ -1,8 +1,17 @@
+import TypingText from "../components/TypingText.jsx";
 
 
 function Home() {
     return (
-        <h1>Home</h1>
+        <TypingText
+            phrases={[
+                "Engineer, Developer.",
+                "Innovator.",
+                "Designer."
+            ]}
+            speed={80}
+            pause={1200}
+        />
     )
 }
 export default Home;
